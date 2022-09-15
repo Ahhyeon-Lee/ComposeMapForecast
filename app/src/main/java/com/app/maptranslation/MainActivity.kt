b63076c2059a78b2811be41b4a38752819ff589f
Ahhyeon-Lee <gda03307@gmail.com>
2022-09-15 18:18:37 +0900

    [화면] HomeScreen, MapScreen 만드는중

--- app/src/main/java/com/app/maptranslation/MainActivity.kt
+++ app/src/main/java/com/app/maptranslation/MainActivity.kt
@@ -8,36 +8,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.app.maptranslation.composables.MyApp
 import com.app.maptranslation.ui.theme.Map_translationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Map_translationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Map_translationTheme {
-        Greeting("Android")
     }
 }
